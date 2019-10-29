a513f19317708c2092456c5472636051650db65c
gbo252 <guy.owen@virginmedia.com>
2019-10-29 18:45:00 +0000

    add validation functions to forms

--- client/src/components/NewCategory.js
+++ client/src/components/NewCategory.js
@@ -3,12 +3,27 @@ import { connect } from "react-redux";
 import { reduxForm, Field } from "redux-form";
 import { newCategory } from "../actions";
 
-const NewCategory = ({ handleSubmit, newCategory, history }) => {
-	const renderInput = ({ input, label }) => {
+let categoriesState = [];
+
+const NewCategory = ({ handleSubmit, newCategory, history, categories }) => {
+	categoriesState = categories;
+
+	const renderError = ({ error, touched }) => {
+		if (error && touched) {
+			return (
+				<div>
+					<span className="helper-text red-text">{error}</span>
+				</div>
+			);
+		}
+	};
+
+	const renderInput = ({ input, label, meta }) => {
 		return (
 			<div>
 				<label>{label}</label>
 				<input {...input} autoComplete="off" />
+				{renderError(meta)}
 			</div>
 		);
 	};
@@ -18,6 +33,7 @@ const NewCategory = ({ handleSubmit, newCategory, history }) => {
 			onSubmit={handleSubmit(formValues =>
 				newCategory(formValues, history)
 			)}
+			style={{ marginTop: "40px" }}
 		>
 			<Field
 				component={renderInput}
@@ -29,11 +45,38 @@ const NewCategory = ({ handleSubmit, newCategory, history }) => {
 	);
 };
 
+const validateCategory = category => {
+	const categoryNames = categoriesState.map(x => x.category.toLowerCase());
+
+	if (categoryNames.includes(category.toLowerCase())) {
+		return `There is already a category called ${category}`;
+	}
+
+	return null;
+};
+
+const validate = formValues => {
+	const errors = {};
+
+	errors.category = validateCategory(formValues.category || "");
+
+	if (!formValues.category) {
+		errors.category = "You must enter a category name";
+	}
+
+	return errors;
+};
+
+const mapStateToProps = ({ categories }) => {
+	return { categories };
+};
+
 export default reduxForm({
-	form: "category"
+	form: "category",
+	validate
 })(
 	connect(
-		null,
+		mapStateToProps,
 		{ newCategory }
 	)(NewCategory)
 );
--- client/src/components/NewFlashCard.js
+++ client/src/components/NewFlashCard.js
@@ -1,14 +1,45 @@
 import React from "react";
 import { connect } from "react-redux";
 import { reduxForm, Field } from "redux-form";
-import { newFlashCard } from "../actions";
+import * as actions from "../actions";
 
-const NewFlashCard = ({ handleSubmit, newFlashCard, match, history }) => {
-	const renderInput = ({ input, label }) => {
+let flashCardsState = [];
+
+const NewFlashCard = ({
+	flashCards,
+	match,
+	fetchFlashCards,
+	clearFlashCards,
+	handleSubmit,
+	newFlashCard,
+	history
+}) => {
+	flashCardsState = flashCards;
+
+	React.useEffect(() => {
+		fetchFlashCards(match.params.category);
+
+		return () => {
+			clearFlashCards();
+		};
+	}, [fetchFlashCards, clearFlashCards, match]);
+
+	const renderError = ({ error, touched }) => {
+		if (error && touched) {
+			return (
+				<div>
+					<span className="helper-text red-text">{error}</span>
+				</div>
+			);
+		}
+	};
+
+	const renderInput = ({ input, label, meta }) => {
 		return (
 			<div>
 				<label>{label}</label>
 				<input {...input} autoComplete="off" />
+				{renderError(meta)}
 			</div>
 		);
 	};
@@ -18,19 +49,53 @@ const NewFlashCard = ({ handleSubmit, newFlashCard, match, history }) => {
 			onSubmit={handleSubmit(formValues =>
 				newFlashCard(formValues, match.params.category, history)
 			)}
+			style={{ marginTop: "40px" }}
 		>
 			<Field component={renderInput} label="Title" name="header" />
 			<Field component={renderInput} label="Content" name="content" />
-			<button className="btn lime black-text right">New Flash Card</button>
+			<button className="btn lime black-text right">
+				New Flash Card
+			</button>
 		</form>
 	);
 };
 
+const validateHeader = header => {
+	const headerNames = flashCardsState.map(x => x.header.toLowerCase());
+
+	if (headerNames.includes(header.toLowerCase())) {
+		return `There is already a flash card with header ${header}`;
+	}
+
+	return null;
+};
+
+const validate = formValues => {
+	const errors = {};
+
+	errors.header = validateHeader(formValues.header || "");
+
+	if (!formValues.header) {
+		errors.header = "You must enter a header";
+	}
+
+	if (!formValues.content) {
+		errors.content = "You must enter some content";
+	}
+
+	return errors;
+};
+
+const mapStateToProps = ({ flashCards }) => {
+	return { flashCards };
+};
+
 export default reduxForm({
-	form: "flashCard"
+	form: "flashCard",
+	validate
 })(
 	connect(
-		null,
-		{ newFlashCard }
+		mapStateToProps,
+		actions
 	)(NewFlashCard)
 );
