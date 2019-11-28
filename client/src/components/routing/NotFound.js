75124ee0a458ba91ee3f2540d0629c755a4c47d3
gbo252 <guy.owen@virginmedia.com>
2019-11-28 21:54:06 +0000

    design 404 component

--- client/src/components/routing/NotFound.js
+++ client/src/components/routing/NotFound.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = ({ location, history }) => {
 	if (location.pathname !== "/404") {
@@ -6,8 +7,14 @@ const NotFound = ({ location, history }) => {
 	}
 
 	return (
-		<div className="display-4 text-center" style={{ marginTop: "100px" }}>
-			404 PAGE NOT FOUND
+		<div className="mx-2 text-center" style={{ marginTop: "100px" }}>
+			<h1 className="h3 font-weight-light mb-3">Page Not Found</h1>
+			<p className="h5 font-weight-light mb-4">
+				Sorry, the page you were looking for does not exist.
+			</p>
+			<Link to="/" className="btn btn-secondary rounded-pill">
+				Home
+			</Link>
 		</div>
 	);
 };
